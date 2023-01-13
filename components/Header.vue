bdeba3775aafca2bfe0cfde997962c9c81b17fd5
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2023-01-14 04:54:51 +0500

    /

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
-<template>
-  <div class = "conteiner">
-  <header>
-    <nav class="navbar bg-light">
+<template >
+  <div class = "conteiner" >
+  <header >
+    <nav class="navbar bg-light" >
       <div class="container-fluid">
         <a class="navbar-brand text-dark ml-5" href="/">IT-proj</a>
       </div>
