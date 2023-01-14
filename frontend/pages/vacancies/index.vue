d0a3fdaf19c8007b0defcf8c453fdfa3f231c6c2
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2023-01-14 18:46:37 +0500

    connect with api

--- frontend/pages/vacancies/index.vue
+++ frontend/pages/vacancies/index.vue
@@ -1,3 +1,18 @@
 <template>
 
 </template>
+
+
+<script>
+  import axios from "axios";
+export default {
+  async asyncData(ctx) {
+    const { data } = await axios.get(`http://127.0.0.1:8000/api/home/`);
+    return {
+      posts: data[0]
+    }
+  }
+}
+
+
+</script>
