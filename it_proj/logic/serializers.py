11b5b6786bafb248f7aea3bd8dafa2aafb5ded6c
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2023-01-14 13:51:49 +0500

    RESTART

--- it_proj/logic/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Descriptions
-
-class DescriptionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Descriptions
-        fields = ('name', 'content')
