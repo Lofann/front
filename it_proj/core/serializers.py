79a858e9285482e5fbde221c70af0972c8b591b0
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2023-01-14 16:45:02 +0500

    add new app and models

---
+++ it_proj/core/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Home, Demand, Skills, Geography
+
+class HomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Home
+        fields = ('title', 'text', 'img')
+
+class DemandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Demand
+        fields = ('title', 'text', 'graph_first', 'graph_second',)
+
+class GeographySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Geography
+        fields = ('title', 'text', 'table', 'graph')
+
+class SkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skills
+        fields = ('title', 'text', 'img')
+
+
